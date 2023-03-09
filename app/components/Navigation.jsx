ac25a37aed3affde523d3ec69f7ce4b27656941b
Byron Calvete <calvete94@gmail.com>
2023-03-09 18:56:22 -0300

    add: navigation component, css styles globals and css modules

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import styles from './Navigation.module.css'
+
+const links = [{
+  label: 'Home',
+  route: '/'
+}, {
+  label: 'About',
+  route: '/about'
+}]
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul className={styles.navigation}>
+        {links.map(({ label, route }) => (
+          <li key={route}>
+            <Link href={route}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation
