9700cc1f471d6914822668c219f48b3a64058fb0
Byron Calvete <calvete94@gmail.com>
2023-03-09 15:10:56 -0300

    Basic configuration and simple navigation from client side

---
+++ app/about/page.jsx
@@ -0,0 +1,7 @@
+const AboutPage = () => {
+  return (
+    <h2>This is an About Page</h2>
+  )
+}
+
+export default AboutPage
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+const MyHome = () => {
+  return (
+    <h1>This is My Home Page</h1>
+  )
+}
+
+export default MyHome
